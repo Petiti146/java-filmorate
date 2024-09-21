aec1d2514e107ca7e28b6c76c2722ee66fbb0234
User Petyan <petiti14771@mail.ru>
2024-09-22 02:44:10 +0300

    The work is almost complete 4

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -6,12 +6,20 @@ import java.util.List;
 
 public interface UserStorage {
     User addUser(User newUser);
+
     void addFriend(long userId, long friendId);
+
     List<User> getUsers();
+
     User getUser(long userId);
+
     int getCommonFriends(long userId, long otherUserId);
+
     public List<User> getFriends(long userId);
+
     User updateUser(User updatedUser);
+
     User deleteUser(long userId);
+
     void removeFriend(long userId, long friendId);
 }
